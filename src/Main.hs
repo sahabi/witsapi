cc85951e0425c921d055e14edf34896aa978f6db
Mohammed Alshiekh <sahabi@gmail.com>
2018-02-13 17:28:41 -0600

    added the function for retrieving the list of indicators from a file

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,28 @@
-import Data.Aeson
-import Data.Text
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Csv as C
+import qualified Data.Vector as V
+
+import Data.Maybe
+import Data.Traversable
+import Data.Aeson.Types (Parser, withObject)
+import Data.Aeson (FromJSON, ToJSON, Object, Value, parseJSON, eitherDecode)
+import qualified Data.Text as T
+import qualified Data.HashMap.Strict as HM
 import Control.Applicative
 import Control.Monad
 import qualified Data.ByteString.Lazy as B
 import Network.HTTP.Conduit (simpleHttp)
 import GHC.Generics
+
+newtype TradeData = TradeData { dataSets :: [Object]
+                           } deriving (Generic, Show)
+
+instance FromJSON TradeData
+instance ToJSON TradeData
 
 data Dataset = Trade | Tariff | Development
   deriving (Show, Enum)
@@ -24,7 +42,7 @@ data Request = Request { dataset :: Dataset
                        , partner :: String
                        , indicator :: String
                        , year :: Int
-                       }
+                       } deriving (Show)
 
 -- this function generates the URL from the request
 getRequestUrl :: Request -> String
@@ -42,12 +60,30 @@ getData :: Request -> IO B.ByteString
 getData r = simpleHttp url
   where url = getRequestUrl r
 
+getIndicators = Just ["XPRT-TRD-VL"]
 -- Example: Dataset: Trade; Reporter: USA; Product: Ores and Metals; Type: JSON...
-r = Request Trade USA "OresMtls" JSON "ALL" "XPRT-TRD-VL" 2016
 
-main :: IO ()
+newtype Indic = Indic {
+  indicatorCode :: String
+  } deriving (Show, Generic)
+
+instance C.FromNamedRecord Indic where
+    parseNamedRecord r = Indic <$> r C..: "indicatorCode"
+
+getInputList :: (C.FromNamedRecord a) => String -> (a -> String) -> IO [String]
+getInputList f p = do
+  csvData <- BL.readFile f
+  case C.decodeByName csvData of
+    Left err -> return []
+    Right (_, v) -> return $ foldr ((:) . p) [] v
+
+req :: IO [Request]
+req = do
+  indList <- getInputList "data/indicatorList.csv" indicatorCode
+  return $ Request <$> [Trade ..] <*> [USA] <*> ["OresMtls"] <*> [JSON] <*> ["ALL"] <*>
+    indList <*> [2000 .. 2016]
+
 main = do
- d <- (eitherDecode <$> getData r) :: IO (Either String Object)
- case d of
-  Left err -> putStrLn err
-  Right ps -> print ps
+  rs <- req
+  d <- sequence (getData <$> rs) -- :: IO (Either String [TradeData])
+  print $ length rs
